2bcbdd8b74a01b8c98c19ff9bbdd270855906375
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2026-08-28 09:40:13 -0300

    refactor: changed link colors

--- src/pages/signin/SigninForm.tsx
+++ src/pages/signin/SigninForm.tsx
@@ -35,7 +35,7 @@ export default function Signin() {
             setFeedback({ type: 'success', message: 'Signed in successfully!' });
 
             setTimeout(() => {
-                navigate('/homepage'); 
+                navigate('/dashboard'); 
             }, 1000);
         } catch (err: unknown) {
             const errorMessage = err instanceof Error ? err.message : 'An unexpected error occurred.';
