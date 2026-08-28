1dcac8486e176e0bc7760be5fe9fe823c6cac4af
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2026-08-28 09:42:28 -0300

    Revert "feat: landing-page-design"
    This reverts commit b633abea8a75e3200cf8f410ba1d20224e8d01a3.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,15 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from '../features/components/signup/SignupForm';
 import Signin from '../features/components/signin/SigninForm';
 import '../components/styles/app.css';
-import LandingPage from '../features/components/landingpage/Landingpage';
 
 function App() {   
   return (     
     <Routes>       
-      <Route path="/landingpage" element={<LandingPage />} />       
-
       <Route path="/auth/register" element={<Signup />} />       
       
       <Route path="/auth/login" element={<Signin />} />
 
-      <Route path="/" element={<Navigate to="/landingpage" replace />} />     
+      <Route path="/" element={<Navigate to="/auth/login" replace />} />     
     </Routes>   
   ); 
 }
