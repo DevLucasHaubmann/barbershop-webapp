f7942209fad894358c9ac062af722514f81b3e51
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2026-08-28 09:42:28 -0300

    Revert "feat: implemented route into the app.tsx file and signupform.tsx.."
    This reverts commit 235a1babf210f55666988fe4c42520b561012a10.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './components/signup/SignupForm';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/auth/register" element={<Signup />} />
-      <Route path="/" element={<Navigate to="/auth/register" replace />} />
-    </Routes>
+    <div>
+      <Signup/>
+    </div>
   );
 }
 
